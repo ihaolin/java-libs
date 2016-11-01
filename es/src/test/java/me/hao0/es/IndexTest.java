9850d43be4392aa9c6d5ae46bd9795ce8d3611a6
haolin <haolin.h0@gmail.com>
2016-11-01 18:31:02 +0800

    add es

---
+++ es/src/test/java/me/hao0/es/IndexTest.java
@@ -0,0 +1,23 @@
+package me.hao0.es;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Author: haolin
+ * Email:  haolin.h0@gmail.com
+ */
+public class IndexTest {
+
+
+
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+    public void create(){
+
+    }
+}
